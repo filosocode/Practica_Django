e4e92515ecabc6781aa2181d2874b010021ab3b3
91andres <91andresmunoz@gmail.com>
2025-07-14 08:44:44 -0500

    segunda parte backend

--- producto/models.py
+++ producto/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
+
 # Create your models here.
+class Producto(models.Model):
+    nombre = models.CharField(max_length=100)
+    precio = models.FloatField()
+    descripcion = models.TextField()
+    imagen = models.ImageField(upload_to="productos/", null=True, blank=True)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.nombre
